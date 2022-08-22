56abec0edfb7d6ecc70fd84615b36962c2b86307
Maria Popa <maria_olimp@yahoo.com>
2022-08-22 20:57:57 +0300

    error route

--- src/components/List-owner.jsx
+++ src/components/List-owner.jsx
@@ -1,11 +1,22 @@
 import Grid from '@mui/material/Grid';
 import NavBar from './NavBar';
+import Typography from '@mui/material/Typography';
 
 export default function ListOwner () {
     return(
         <Grid container sx={{ flexGrow: 1 }}>
             <Grid item xs={12} md={12} lg={12}>
                 <NavBar />
+            </Grid>
+            <Grid
+                item
+                xs={12}
+                sx={{
+                    display: 'flex',
+                    alignItems: 'flex-start'
+                }}
+            >
+                <Typography variant='h2' gutterBottom sx={{ m: { xs: 2 } }}>Birthday</Typography>
             </Grid>
         </Grid>
     );
